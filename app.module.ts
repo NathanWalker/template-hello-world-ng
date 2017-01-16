5dcd4b03f941facc553a151824ca69a35af1367e
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-15 20:49:32 -0800

    simplified for ngx-magic

--- app.module.ts
+++ app.module.ts
@@ -1,11 +1,7 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
-import { AppComponent } from "./app.component";
-
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
 
 @NgModule({
     bootstrap: [
@@ -16,12 +12,7 @@ import { ItemDetailComponent } from "./item/item-detail.component";
         AppRoutingModule
     ],
     declarations: [
-        AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
-    ],
-    providers: [
-        ItemService
+        AppComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
