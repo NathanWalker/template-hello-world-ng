5d3f80d15bf8903b384f7ec99451cc2c0f6dfd9a
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-15 21:26:11 -0800

    simplified for ngx-magic

--- app.module.ts
+++ app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app/app.component';
 
 @NgModule({
@@ -8,8 +7,7 @@ import { AppComponent } from './app/app.component';
         AppComponent
     ],
     imports: [
-        NativeScriptModule,
-        AppRoutingModule
+        NativeScriptModule
     ],
     declarations: [
         AppComponent
